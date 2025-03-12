395f81b392a2940a6865bcf892d07382a4469c96
Artem Pechenkin <tema_045@mail.ru>
2025-03-12 18:31:27 +0500

    init commit

--- src/test/java/WebHooks.java
+++ src/test/java/WebHooks.java
@@ -1,7 +1,9 @@
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Step;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import io.qameta.allure.selenide.AllureSelenide;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
@@ -12,6 +14,11 @@ public class WebHooks {
     public void openBrowser() {
         Selenide.open("https://edujira.ifellow.ru");
         getWebDriver().manage().window().maximize();
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true)
+        );
     }
 
     @AfterEach
